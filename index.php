344ef3e947d79b7fdaffc89003641cc344014e5c
meliBruve <melibruve@gmail.com>
2021-02-09 16:43:12 -0300

    meli_09/02
    carga de archivos de home (css y js)

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link href="./view/css/bootstrap/bootstrap.min.css" rel="stylesheet">
+	<link href="./view/css/style.css" rel="stylesheet">
+</head>
+<body>
+
+<section id="hero">
+
+    <div class="container">
+      <div class="row" style="min-height: 445px">
+        <div class="col-lg-7 pt-5 pt-lg-0 order-2 order-lg-1 d-flex align-items-center" style="z-index: 999">
+          <div data-aos="zoom-out">
+            <h1>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </h1>
+              <a  href="contacto.html" class="btn-get-started scrollto btn-login mr-2">Iniciar sesión</a>
+              <a  href="contacto.html" class="btn-get-started scrollto btn-registro mr-2">Registrarse</a>
+              
+          </div>
+          <br>
+        </div>
+      </div>
+    </div>
+
+    <svg class="hero-waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28 " preserveAspectRatio="none">
+      <defs>
+        <path id="wave-path" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z">
+      </defs>
+      <g class="wave1">
+        <use xlink:href="#wave-path" x="50" y="3" fill="rgba(255,255,255, .1)">
+      </g>
+      <g class="wave2">
+        <use xlink:href="#wave-path" x="50" y="0" fill="rgba(255,255,255, .2)">
+      </g>
+      <g class="wave3">
+        <use xlink:href="#wave-path" x="50" y="9" fill="#fff">
+      </g>
+    </svg>
+
+</section><!-- End Hero -->
+
+<section id="tips">
+  <div class="container">
+    <div class="row justify-content-between">
+      <div class="col-md-3 col-sm-6 col-12 card-tips">
+        <h3><b>Paso uno</b></h3>
+        <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has</p>
+      </div>
+
+      <div class="col-md-3 col-sm-6 col-12 card-tips">
+        <h3><b>Paso dos</b></h3>
+        <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has</p>
+      </div>
+
+      <div class="col-md-3 col-sm-6 col-12 card-tips">
+        <h3><b>Paso tres</b></h3>
+        <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has</p>
+      </div>
+
+    </div>
+    
+  </div>
+</section>
+
+<!--incluir php-->
+<section id="footer">
+  <div class="col-2">
+    <span>Insttitucional</span>
+    <div>
+      <ul>
+        <li><a href="#">Nosotros</a></li>
+        <li><a href="#">Terminos y condiciones</a></li>
+        <li><a href="#">Políticas de privacidad</a></li>
+      </ul>
+    </div>
+  </div>
+
+  <div class="col-2">
+    <span>Útiles</span>
+    <div>
+      <ul>
+        <li><a href="#">Como comprar/a></li>
+        <li><a href="#">medios de pago</a></li>
+        <li><a href="#">Políticas de privacidad</a></li>
+      </ul>
+    </div>
+  </div>
+</section>
+</body>
+
+  <script src="./view/jquery/jquery.min.js"></script>
+  <script src="./view/js/bootstrap/bootstrap.bundle.min.js"></script>
+</html>
